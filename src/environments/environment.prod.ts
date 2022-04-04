9755773608f559752f68b8fa73caae857006565f
Jijo A J <love4messi@gmail.com>
2022-04-04 13:36:41 +0530

    prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: true,
-  processDetailURL:'/ProcessDetail',
-  completeProcessingURL:'/CompleteProcessing',
-  tokenURL:'/oauth/token',
-  tokenURL2:'http://localhost:44446/oauth/token',
-  signup:'/signup'
+  processDetailURL: '/ProcessDetail',
+  completeProcessingURL: '/CompleteProcessing',
+  tokenURL: '/oauth/token',
+  signup: '/signup'
 
 };
